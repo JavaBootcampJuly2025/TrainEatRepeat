a427a0c41807428052739844445a179e5b252d7f
shokkZED <shokk685@gmail.com>
2025-07-15 21:47:26 +0200

    TER-10 Implemented logic related to exercise posting

---
+++ train-eat-repeat/src/main/java/org/athletes/traineatrepeat/model/request/ExerciseRequest.java
@@ -0,0 +1,6 @@
+package org.athletes.traineatrepeat.model.request;
+
+public record ExerciseRequest(
+        String name,
+        float MET
+) {}
---
+++ train-eat-repeat/src/main/java/org/athletes/traineatrepeat/repository/ExerciseRepository.java
@@ -0,0 +1,13 @@
+package org.athletes.traineatrepeat.repository;
+
+import org.athletes.traineatrepeat.repository.dto.ExerciseDTO;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class ExerciseRepository {
+
+    public ExerciseDTO save(ExerciseDTO dto) {
+        // TODO: Implement save to database logic
+        return dto;
+    }
+}
