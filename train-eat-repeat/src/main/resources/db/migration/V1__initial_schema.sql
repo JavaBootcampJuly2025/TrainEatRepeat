ead75b19e84530434cc0ecf88d657f61ac1f174d
Georgijs Cibulskis <cibulsis@gmail.com>
2025-07-16 17:04:02 +0300

    TER-12 Created tables and inserted values

--- train-eat-repeat/src/main/resources/db/migration/V1__initial_schema.sql
+++ train-eat-repeat/src/main/resources/db/migration/V1__initial_schema.sql
@@ -11,3 +11,35 @@ CREATE TABLE USERS
     BMR      FLOAT,
     role     VARCHAR(255)
 );
+
+CREATE TABLE mealrecords
+(
+    id        VARCHAR(36) PRIMARY KEY,
+    user_uuid VARCHAR(36),
+    food_name VARCHAR(255),
+    calories  FLOAT,
+    carbs     FLOAT,
+    protein   FLOAT,
+    fat       FLOAT,
+    date      DATE,
+    CONSTRAINT fk_user FOREIGN KEY (user_uuid) REFERENCES users(uuid) ON DELETE CASCADE
+);
+
+
+CREATE TABLE trainingrecords
+(
+    id   VARCHAR(36) PRIMARY KEY,
+    user_uuid     VARCHAR(36),
+    exercise      VARCHAR(255),
+    duration      FLOAT,
+    calories_lost FLOAT,
+    date          DATE,
+    CONSTRAINT fk_training_user FOREIGN KEY (user_uuid) REFERENCES users(uuid) ON DELETE CASCADE
+);
+
+CREATE TABLE exercises
+(
+    uuid VARCHAR(36) PRIMARY KEY,
+    name VARCHAR(255),
+    met  FLOAT
+);
