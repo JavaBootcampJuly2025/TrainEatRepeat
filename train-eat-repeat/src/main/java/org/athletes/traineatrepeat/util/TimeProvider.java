684bc62075e205fb7df8317ca297692f5086c8b7
Dziugasn <dziugas.naimavicius@stud.vilniustech.lt>
2025-07-21 15:31:57 +0300

    TER-39 Introduced TimeProvider component in TrainingService class

--- train-eat-repeat/src/main/java/org/athletes/traineatrepeat/util/TimeProvider.java
+++ train-eat-repeat/src/main/java/org/athletes/traineatrepeat/util/TimeProvider.java
@@ -1,11 +1,23 @@
 package org.athletes.traineatrepeat.util;
 
 import java.time.LocalDate;
+import java.util.Optional;
 import org.springframework.stereotype.Component;
 
 @Component
 public class TimeProvider {
+
+    private Optional<LocalDate> overriddenDate = Optional.empty();
+
     public LocalDate getCurrentDate() {
-        return LocalDate.now();
+        return overriddenDate.orElse(LocalDate.now());
+    }
+
+    public void setCurrentDate(LocalDate date) {
+        this.overriddenDate = Optional.of(date);
+    }
+
+    public void reset() {
+        this.overriddenDate = Optional.empty();
     }
 }
