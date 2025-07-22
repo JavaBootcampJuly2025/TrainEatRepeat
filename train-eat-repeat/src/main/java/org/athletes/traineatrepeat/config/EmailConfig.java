b40cdec01daa258953f7bde43447c5eab9d3bb66
Georgijs Cibulskis <cibulsis@gmail.com>
2025-07-22 17:06:30 +0300

    TER-51 Hidden email app password into a variable

--- train-eat-repeat/src/main/java/org/athletes/traineatrepeat/config/EmailConfig.java
+++ train-eat-repeat/src/main/java/org/athletes/traineatrepeat/config/EmailConfig.java
@@ -1,5 +1,6 @@
 package org.athletes.traineatrepeat.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -10,13 +11,19 @@ import java.util.Properties;
 @Configuration
 public class EmailConfig {
 
+    @Value("${spring.mail.username}")
+    private String username;
+
+    @Value("${spring.mail.password}")
+    private String password;
+
     @Bean
     public JavaMailSender javaMailSender() {
         JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
         mailSender.setHost("smtp.gmail.com");
         mailSender.setPort(587);
-        mailSender.setUsername("traineatrepeatapp@gmail.com"); // Replace with your email
-        mailSender.setPassword("wspdvcswoxjzbdat"); // Replace with your app-specific password
+        mailSender.setUsername(username);
+        mailSender.setPassword(password);
 
         Properties props = mailSender.getJavaMailProperties();
         props.put("mail.transport.protocol", "smtp");
