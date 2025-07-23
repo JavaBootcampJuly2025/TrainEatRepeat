f5d92e5814b95051544ddf48be23606e1fcbb729
Georgijs Cibulskis <cibulsis@gmail.com>
2025-07-23 13:39:58 +0300

    Cleaned up the login/register features (no validation and logic in controller)

--- train-eat-repeat/src/main/java/org/athletes/traineatrepeat/model/request/RegisterRequest.java
+++ train-eat-repeat/src/main/java/org/athletes/traineatrepeat/model/request/RegisterRequest.java
@@ -1,25 +1,16 @@
 package org.athletes.traineatrepeat.model.request;
 
-import jakarta.validation.constraints.*;
-import org.athletes.traineatrepeat.validation.PasswordMatches;
-
-@PasswordMatches
 public record RegisterRequest(
-    @NotEmpty(message = "Username is required") String username,
-    @NotEmpty(message = "Email is required") @Email(message = "Invalid email format") String email,
-    @NotEmpty(message = "Password is required")
-        @Size(min = 9, message = "Minimum password length is 9 characters")
+        String username,
+        String email,
         String password,
-    @NotEmpty(message = "Confirm password is required") String confirmPassword,
-    @NotNull(message = "Age is required") @Min(value = 1, message = "Age must be positive")
+        String confirmPassword,
         Integer age,
-    @NotEmpty(message = "Gender is required") String gender,
-    @NotNull(message = "Weight is required") @Min(value = 1, message = "Weight must be positive")
+        String gender,
         Float weight,
-    @NotNull(message = "Height is required") @Min(value = 1, message = "Height must be positive")
         Float height) {
 
-  public static RegisterRequest ofInitialRequestForm() {
-    return new RegisterRequest("", "", "", "", null, "", null, null);
-  }
+    public static RegisterRequest ofInitialRequestForm() {
+        return new RegisterRequest("", "", "", "", null, "", null, null);
+    }
 }
